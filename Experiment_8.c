72ca28b9b9175bc8bb79d635724922e8cec2a714
SamridhaShri <shrisamridha@gmail.com>
2026-04-12 22:23:23 +0530

    exp 8

--- Experiment_8.c
+++ Experiment_8.c
@@ -1,41 +1,105 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-// function f to exit from the loop
-// if given condition is not true
-void f()
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char productions[10][20];
+char leading[10][10], trailing[10][10];
+int n;
+
+// function to find LEADING
+void findLeading(int i, int j)
 {
-printf("Not operator grammar");
-exit(0);
+    if (!isupper(productions[i][j])) // terminal
+    {
+        int k;
+        for (k = 0; k < strlen(leading[i]); k++)
+            if (leading[i][k] == productions[i][j])
+                return;
+
+        int len = strlen(leading[i]);
+        leading[i][len] = productions[i][j];
+        leading[i][len + 1] = '\0';
+    }
+    else
+    {
+        int k;
+        for (k = 0; k < n; k++)
+        {
+            if (productions[k][0] == productions[i][j])
+            {
+                findLeading(k, 2);
+            }
+        }
+    }
 }
+
+// function to find TRAILING
+void findTrailing(int i, int j)
 {
-void main()
-char grm[20][20], c;
-// Here using flag variable,
-// considering grammar is not operator grammar
-int i, n, j = 2, flag = 0;
-// taking number of productions from user
-scanf("%d", &n);
-for (i = 0; i < n; i++)
-scanf("%s", grm[i]);
-for (i = 0; i < n; i++) {
-c = grm[i][2];
-while (c != '\0') {
-if (grm[i][3] == '+' || grm[i][3] == '-
-'
-|| grm[i][3] == '*' || grm[i][3] == '/')
-flag = 1;
-else {
-flag = 0;
-f();
+    if (!isupper(productions[i][j])) // terminal
+    {
+        int k;
+        for (k = 0; k < strlen(trailing[i]); k++)
+            if (trailing[i][k] == productions[i][j])
+                return;
+
+        int len = strlen(trailing[i]);
+        trailing[i][len] = productions[i][j];
+        trailing[i][len + 1] = '\0';
+    }
+    else
+    {
+        int k;
+        for (k = 0; k < n; k++)
+        {
+            if (productions[k][0] == productions[i][j])
+            {
+                findTrailing(k, strlen(productions[k]) - 1);
+            }
+        }
+    }
 }
-if (c == '$') {
-flag = 0;
-f();
-}
-c = grm[i][++j];
-}
-}
-if (flag == 1)
-printf("Operator grammar");
+
+int main()
+{
+    int i;
+
+    printf("Enter number of productions: ");
+    scanf("%d", &n);
+
+    printf("Enter productions (Example: E=E+T):\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%s", productions[i]);
+        leading[i][0] = '\0';
+        trailing[i][0] = '\0';
+    }
+
+    // compute LEADING
+    for (i = 0; i < n; i++)
+    {
+        findLeading(i, 2);
+    }
+
+    // compute TRAILING
+    for (i = 0; i < n; i++)
+    {
+        findTrailing(i, strlen(productions[i]) - 1);
+    }
+
+    // display LEADING
+    printf("\nLEADING:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%c : { %s }\n", productions[i][0], leading[i]);
+    }
+
+    // display TRAILING
+    printf("\nTRAILING:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%c : { %s }\n", productions[i][0], trailing[i]);
+    }
+
+    return 0;
 }
